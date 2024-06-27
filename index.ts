c96ebd3e291fba570c8b3fa4941f2e0a5c5001d8
Gregory Brown <brown.gregm@gmail.com>
2024-06-27 16:43:56 -0700

    simplest create log function

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
-export function createLog() {
+export function createLog(apiKey: string, response: string) {
   console.log("Will create log in Melodi");
+  fetch(`https://app.melodi.fyi/api/external/logs?apiKey=${apiKey}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data: { response },
+    }),
+  });
 }
