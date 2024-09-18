ee0cb5aede61ed6f8b72140431a6919966e7b77b
Gregory Brown <brown.gregm@gmail.com>
2024-09-17 22:23:42 -0700

    use the utils stuff to cleanup feedback request

--- feedback.ts
+++ feedback.ts
@@ -1,43 +1,10 @@
 import { CreateFeedbackRequest } from "./types";
+import { post } from "./utils";
 
 export async function createFeedback(
   createFeedbackRequest: CreateFeedbackRequest,
   apiKey?: string
 ) {
-  const key = apiKey || process.env.MELODI_API_KEY;
-
-  if (!key) {
-    throw new Error(
-      "Either pass apiKey as an argument or set MELODI_API_KEY in your environment variables"
-    );
-  }
-
-  const response = await fetch(
-    `https://app.melodi.fyi/api/external/feedback?apiKey=${key}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(createFeedbackRequest),
-    }
-  );
-
-  if (response.status !== 200) {
-    let body = "";
-    try {
-      body = await response.json();
-    } catch (e) {
-      // pass
-    }
-    throw new Error(
-      `An error occurred creating the feedback: status ${
-        response.status
-      }, response body: ${JSON.stringify(body)}`
-    );
-  }
-
-  const log = await response.json();
-
-  return log;
+  const feedback = await post("feedback", createFeedbackRequest, apiKey);
+  return feedback;
 }
